087343c807784b3a8f569e3117402b643ae639d2
Ola Folkvord <of@novotech.no>
2021-09-10 17:16:05 +0200

    new login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,22 +1,40 @@
 import { useState, FormEvent } from "react";
 import { useMsal } from "@azure/msal-react";
-import { useTheme, Box, Paper, Typography, Button } from "@material-ui/core";
+import {
+    useTheme,
+    Box,
+    Button,
+    OutlinedInput,
+    IconButton,
+    InputAdornment,
+    FormControl,
+    makeStyles,
+} from "@material-ui/core";
 
-import { TextField } from "components";
 import { loginRequest } from "config/auth";
 import { useAppDispatch } from "app/store";
 import { authActions } from "slices/authSlice";
 import { login, storeToken } from "utils/auth";
 
-// NOTE(OLA): Pending design
+import { Lock, Visibility, VisibilityOff, AccountCircle } from "@material-ui/icons";
+import { ReactComponent as NovorenderLogo } from "media/img/novorender_logo_RGB_2021.svg";
+import { useToggle } from "hooks/useToggle";
+
+const useStyles = makeStyles((_theme) => ({
+    button: {
+        textTransform: "none",
+    },
+}));
 
 export function Login() {
     const theme = useTheme();
+    const classes = useStyles();
     const { instance } = useMsal();
     const dispatch = useAppDispatch();
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, toggleShowPassword] = useToggle(false);
 
     const handleAdRedirect = async () => {
         instance.loginRedirect({ ...loginRequest, prompt: "select_account" }).catch((e) => {
@@ -40,53 +58,95 @@ export function Login() {
     };
 
     return (
-        <Box
-            height={"100vh"}
-            bgcolor={theme.palette.secondary.main}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Paper>
-                <Box p={2} minWidth={320}>
+        <Box height={"100vh"} bgcolor={{ xs: theme.palette.common.white, sm: theme.palette.grey[200] }}>
+            <Box p={3} textAlign={{ xs: "center", sm: "left" }}>
+                <NovorenderLogo title="novorender" height={35} fill={theme.palette.primary.main} />
+            </Box>
+
+            <Box
+                p={2}
+                maxWidth={430}
+                maxHeight={{ xs: "auto", sm: 512 }}
+                width={1}
+                height={1}
+                borderRadius={4}
+                bgcolor={theme.palette.common.white}
+                py={{ xs: 2, sm: 8.5 }}
+                px={{ xs: 2, sm: 8 }}
+                mx="auto"
+                mt={{ xs: 0, sm: 13 }}
+            >
+                <Box mb={2} fontSize={24} fontWeight={700} textAlign="center" component="h1">
+                    Log in
+                </Box>
+                <form onSubmit={handleSubmit}>
                     <Box mb={2}>
-                        <Typography variant="h4" component="h1" align="center">
-                            Log in
-                        </Typography>
-                    </Box>
-                    <form onSubmit={handleSubmit}>
-                        <Box mb={1}>
-                            <TextField
-                                fullWidth
-                                variant="outlined"
-                                label="Username"
+                        <FormControl fullWidth>
+                            <label htmlFor="username">Username</label>
+                            <OutlinedInput
+                                id="username"
                                 value={username}
                                 onChange={(e) => setUsername(e.target.value)}
+                                type="text"
+                                startAdornment={
+                                    <InputAdornment position="start">
+                                        <AccountCircle htmlColor={theme.palette.grey[600]} />
+                                    </InputAdornment>
+                                }
                             />
-                        </Box>
-                        <Box mb={1}>
-                            <TextField
-                                fullWidth
-                                variant="outlined"
-                                label="Password"
+                        </FormControl>
+                    </Box>
+                    <Box mb={3}>
+                        <FormControl fullWidth>
+                            <label htmlFor="password">Password</label>
+                            <OutlinedInput
+                                id="password"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
-                                type="password"
+                                type={showPassword ? "text" : "password"}
+                                startAdornment={
+                                    <InputAdornment position="start">
+                                        <Lock htmlColor={theme.palette.grey[600]} />
+                                    </InputAdornment>
+                                }
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={toggleShowPassword}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
                             />
-                        </Box>
-                        <Box display="flex" justifyContent="space-between" flexShrink={0}>
-                            <Button type="button" fullWidth variant="contained" onClick={handleAdRedirect}>
-                                Active Directory
-                            </Button>
-                            <Box width={1} ml={1} flexShrink={1.1}>
-                                <Button type="submit" variant="contained" fullWidth color="secondary">
-                                    Log in
-                                </Button>
-                            </Box>
-                        </Box>
-                    </form>
-                </Box>
-            </Paper>
+                        </FormControl>
+                    </Box>
+                    <Box mb={2}>
+                        <Button
+                            className={classes.button}
+                            type="submit"
+                            variant="contained"
+                            fullWidth
+                            color="primary"
+                            size="large"
+                        >
+                            Log in
+                        </Button>
+                    </Box>
+                    <Button
+                        className={classes.button}
+                        type="button"
+                        fullWidth
+                        variant="outlined"
+                        color="secondary"
+                        size="large"
+                        onClick={handleAdRedirect}
+                    >
+                        Active Directory
+                    </Button>
+                </form>
+            </Box>
         </Box>
     );
 }
