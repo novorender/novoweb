8ff0d9a52f049ac4544a44d94e1a4d56e38430a2
Ola Folkvord <of@novotech.no>
2021-12-07 14:44:41 +0100

    add selected groups to layer. show count and toggle view mode from widget

--- src/features/layers/layers.tsx
+++ src/features/layers/layers.tsx
@@ -1,44 +1,92 @@
 import { AddCircle, DeleteSweep, RemoveCircle } from "@mui/icons-material";
-import { Box, Button, useTheme } from "@mui/material";
+import { Box, Button, useTheme, Typography, RadioGroup, FormControlLabel, Radio } from "@mui/material";
+
+import { useAppDispatch, useAppSelector } from "app/store";
+import { Divider } from "components";
+
+import { customGroupsActions, useCustomGroups } from "contexts/customGroups";
 import { highlightActions, useDispatchHighlighted, useHighlighted } from "contexts/highlighted";
 import { useDispatchVisible, useVisible, visibleActions } from "contexts/visible";
+import { ObjectVisibility, renderActions, selectDefaultVisibility } from "slices/renderSlice";
 
 export function Layers() {
     const theme = useTheme();
+    const defaultVisibility = useAppSelector(selectDefaultVisibility);
     const { idArr: highlighted } = useHighlighted();
     const { idArr: visible } = useVisible();
+    const { state: customGroups, dispatch: dispatchCustomGroups } = useCustomGroups();
 
     const dispatchHighlighted = useDispatchHighlighted();
     const dispatchVisible = useDispatchVisible();
+    const dispatch = useAppDispatch();
+
+    const selectedGroups = customGroups.filter((group) => group.selected);
+    const hasHighlighted = highlighted.length || selectedGroups.length;
 
     const handleAdd = () => {
-        dispatchVisible(visibleActions.add(highlighted));
+        const fromGroup = selectedGroups.map((grp) => grp.ids).flat();
+
+        dispatchVisible(visibleActions.add(highlighted.concat(fromGroup)));
         dispatchHighlighted(highlightActions.setIds([]));
+        dispatchCustomGroups(customGroupsActions.set(customGroups.map((group) => ({ ...group, selected: false }))));
     };
 
     const handleRemove = () => {
-        dispatchVisible(visibleActions.remove(highlighted));
+        const fromGroup = selectedGroups.map((grp) => grp.ids).flat();
+
+        dispatchVisible(visibleActions.remove(highlighted.concat(fromGroup)));
         dispatchHighlighted(highlightActions.setIds([]));
+        dispatchCustomGroups(customGroupsActions.set(customGroups.map((group) => ({ ...group, selected: false }))));
     };
 
     const handleClear = () => {
         dispatchVisible(visibleActions.set([]));
     };
 
+    const handleViewTypeChange = (_: any, value: string) => {
+        const val = Number(value) as ObjectVisibility;
+        dispatch(renderActions.setDefaultVisibility(val));
+    };
+
     return (
-        <Box p={1} boxShadow={theme.customShadows.widgetHeader} display="flex" justifyContent="space-between">
-            <Button color="grey" disabled={!highlighted.length} onClick={handleAdd}>
-                <AddCircle sx={{ mr: 1 }} />
-                Add
-            </Button>
-            <Button color="grey" disabled={!highlighted.length || !visible.length} onClick={handleRemove}>
-                <RemoveCircle sx={{ mr: 1 }} />
-                Remove
-            </Button>
-            <Button color="grey" disabled={!visible.length} onClick={handleClear}>
-                <DeleteSweep sx={{ mr: 1 }} />
-                Clear
-            </Button>
-        </Box>
+        <>
+            <Box p={1} boxShadow={theme.customShadows.widgetHeader} display="flex" justifyContent="space-between">
+                <Button color="grey" disabled={!hasHighlighted} onClick={handleAdd}>
+                    <AddCircle sx={{ mr: 1 }} />
+                    Add
+                </Button>
+                <Button color="grey" disabled={!hasHighlighted || !visible.length} onClick={handleRemove}>
+                    <RemoveCircle sx={{ mr: 1 }} />
+                    Remove
+                </Button>
+                <Button color="grey" disabled={!visible.length} onClick={handleClear}>
+                    <DeleteSweep sx={{ mr: 1 }} />
+                    Clear
+                </Button>
+            </Box>
+            <Box p={1} mt={1}>
+                <Typography sx={{ mb: 2 }}>Objects in layer: {visible.length}</Typography>
+                <Divider sx={{ mb: 2 }} />
+                <Typography fontWeight={600}>View mode</Typography>
+                <RadioGroup
+                    aria-label="View type"
+                    value={defaultVisibility}
+                    onChange={handleViewTypeChange}
+                    name="radio-buttons-group"
+                >
+                    <FormControlLabel value={ObjectVisibility.Neutral} control={<Radio />} label="All" />
+                    <FormControlLabel
+                        value={ObjectVisibility.SemiTransparent}
+                        control={<Radio />}
+                        label="Layer - Semi-transparent"
+                    />
+                    <FormControlLabel
+                        value={ObjectVisibility.Transparent}
+                        control={<Radio />}
+                        label="Layer - Transparent"
+                    />
+                </RadioGroup>
+            </Box>
+        </>
     );
 }
