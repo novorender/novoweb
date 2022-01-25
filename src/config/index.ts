c38707c84a14948ccd575fde1c82b3744eeeacc8
Fighter125 <Fighter125@mail.ru>
2022-01-25 18:23:00 +0300

    back to data.novorender.com

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const dataServerBaseUrl =
-    process.env.REACT_APP_DATA_SERVER_URL || (window as any).dataServerUrl || "https://localhost:5000/api";
+    process.env.REACT_APP_DATA_SERVER_URL || (window as any).dataServerUrl || "https://data.novorender.com/api";
 export const offscreenCanvas = "OffscreenCanvas" in window;
 export const hasCreateImageBitmap = "createImageBitmap" in window;
