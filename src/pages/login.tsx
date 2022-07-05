b0e15c6951d8a04929378b32bc1c58262c71243d
Ola Folkvord <ola.folki@gmail.com>
2022-07-05 11:33:47 +0200

    fix scroll on login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,20 +2,20 @@ import { useState, FormEvent, ChangeEvent } from "react";
 import { useMsal } from "@azure/msal-react";
 import { useTheme, Box, OutlinedInput, IconButton, InputAdornment, FormControl, CircularProgress } from "@mui/material";
 import { useHistory } from "react-router-dom";
+import { LoadingButton } from "@mui/lab";
+import { Lock, Visibility, VisibilityOff, AccountCircle } from "@mui/icons-material";
 
+import { dataServerBaseUrl } from "config";
 import { loginRequest } from "config/auth";
+import { StorageKey } from "config/storage";
 import { useAppDispatch } from "app/store";
 import { authActions } from "slices/authSlice";
 import { login } from "utils/auth";
-import { useToggle } from "hooks/useToggle";
-
-import { Lock, Visibility, VisibilityOff, AccountCircle } from "@mui/icons-material";
+import { ScrollBox } from "components";
 import { ReactComponent as NovorenderLogo } from "media/img/novorender_logo_RGB_2021.svg";
 import { deleteFromStorage, saveToStorage } from "utils/storage";
-import { StorageKey } from "config/storage";
+import { useToggle } from "hooks/useToggle";
 import { useSceneId } from "hooks/useSceneId";
-import { dataServerBaseUrl } from "config";
-import { LoadingButton } from "@mui/lab";
 
 export function Login() {
     const theme = useTheme();
@@ -88,111 +88,117 @@ export function Login() {
     };
 
     return (
-        <Box height={"100vh"} bgcolor={{ xs: theme.palette.common.white, sm: theme.palette.grey[200] }}>
-            <Box p={3} textAlign={{ xs: "center", sm: "left" }}>
-                <NovorenderLogo title="novorender" height={35} fill={theme.palette.primary.main} />
-            </Box>
-
-            <Box
-                p={2}
-                maxWidth={430}
-                maxHeight={{ xs: "auto", sm: 512 }}
-                width={1}
-                height={1}
-                borderRadius="4px"
-                bgcolor={theme.palette.common.white}
-                py={{ xs: 2, sm: 8.5 }}
-                px={{ xs: 2, sm: 8 }}
-                mx="auto"
-                mt={{ xs: 0, sm: 13 }}
-            >
-                <Box mb={2} fontSize={24} fontWeight={700} textAlign="center" component="h1">
-                    Log in
+        <Box
+            display="flex"
+            flexDirection="column"
+            height="100vh"
+            width={1}
+            bgcolor={{ xs: theme.palette.common.white, sm: theme.palette.grey[200] }}
+        >
+            <ScrollBox mb={2}>
+                <Box p={3} textAlign={{ xs: "center", sm: "left" }}>
+                    <NovorenderLogo title="novorender" height={35} fill={theme.palette.primary.main} />
                 </Box>
-                <form onSubmit={handleSubmit}>
-                    <Box mb={2}>
-                        <FormControl fullWidth>
-                            <label htmlFor="username">Username</label>
-                            <OutlinedInput
-                                id="username"
-                                required
-                                value={username}
-                                onChange={handleChange(setUsername)}
-                                type="text"
-                                startAdornment={
-                                    <InputAdornment position="start">
-                                        <AccountCircle htmlColor={theme.palette.grey[600]} />
-                                    </InputAdornment>
-                                }
-                            />
-                        </FormControl>
+
+                <Box
+                    p={2}
+                    maxWidth={430}
+                    width={1}
+                    borderRadius="4px"
+                    bgcolor={theme.palette.common.white}
+                    py={{ xs: 2, sm: 8.5 }}
+                    px={{ xs: 2, sm: 8 }}
+                    mx="auto"
+                    mt={{ xs: 0, sm: 13 }}
+                >
+                    <Box mb={2} fontSize={24} fontWeight={700} textAlign="center" component="h1">
+                        Log in
                     </Box>
-                    <Box mb={1.5} pb={4} position="relative">
-                        <FormControl fullWidth>
-                            <label htmlFor="password">Password</label>
-                            <OutlinedInput
-                                id="password"
-                                required
-                                value={password}
-                                onChange={handleChange(setPassword)}
-                                type={showPassword ? "text" : "password"}
-                                startAdornment={
-                                    <InputAdornment position="start">
-                                        <Lock htmlColor={theme.palette.grey[600]} />
-                                    </InputAdornment>
-                                }
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            aria-label="toggle password visibility"
-                                            onClick={toggleShowPassword}
-                                            size="large"
-                                        >
-                                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                            />
-                        </FormControl>
-                        <Box color={"red"} position="absolute" bottom={0}>
-                            {error}
+                    <form onSubmit={handleSubmit}>
+                        <Box mb={2}>
+                            <FormControl fullWidth>
+                                <label htmlFor="username">Username</label>
+                                <OutlinedInput
+                                    id="username"
+                                    required
+                                    value={username}
+                                    onChange={handleChange(setUsername)}
+                                    type="text"
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <AccountCircle htmlColor={theme.palette.grey[600]} />
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
                         </Box>
-                    </Box>
-                    <Box mb={2}>
+                        <Box mb={1.5} pb={4} position="relative">
+                            <FormControl fullWidth>
+                                <label htmlFor="password">Password</label>
+                                <OutlinedInput
+                                    id="password"
+                                    required
+                                    value={password}
+                                    onChange={handleChange(setPassword)}
+                                    type={showPassword ? "text" : "password"}
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <Lock htmlColor={theme.palette.grey[600]} />
+                                        </InputAdornment>
+                                    }
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={toggleShowPassword}
+                                                size="large"
+                                            >
+                                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                            <Box color={"red"} position="absolute" bottom={0}>
+                                {error}
+                            </Box>
+                        </Box>
+                        <Box mb={2}>
+                            <LoadingButton
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                size="large"
+                                loading={loadingLogin}
+                                loadingIndicator={
+                                    <Box display="flex" alignItems="center">
+                                        Log in <CircularProgress sx={{ ml: 1 }} color="inherit" size={16} />
+                                    </Box>
+                                }
+                            >
+                                Log in
+                            </LoadingButton>
+                        </Box>
                         <LoadingButton
-                            type="submit"
+                            type="button"
                             fullWidth
-                            variant="contained"
-                            color="primary"
+                            variant="outlined"
+                            color="secondary"
                             size="large"
-                            loading={loadingLogin}
+                            onClick={handleAdRedirect}
+                            loading={loadingTenant}
                             loadingIndicator={
                                 <Box display="flex" alignItems="center">
-                                    Log in <CircularProgress sx={{ ml: 1 }} color="inherit" size={16} />
+                                    Active Directory <CircularProgress sx={{ ml: 1 }} color="inherit" size={16} />
                                 </Box>
                             }
                         >
-                            Log in
+                            Active Directory
                         </LoadingButton>
-                    </Box>
-                    <LoadingButton
-                        type="button"
-                        fullWidth
-                        variant="outlined"
-                        color="secondary"
-                        size="large"
-                        onClick={handleAdRedirect}
-                        loading={loadingTenant}
-                        loadingIndicator={
-                            <Box display="flex" alignItems="center">
-                                Active Directory <CircularProgress sx={{ ml: 1 }} color="inherit" size={16} />
-                            </Box>
-                        }
-                    >
-                        Active Directory
-                    </LoadingButton>
-                </form>
-            </Box>
+                    </form>
+                </Box>
+            </ScrollBox>
         </Box>
     );
 }
