4181060f0f333ef8cc3efc8cf4521a823b795c53
Ola Folkvord <ola.folki@gmail.com>
2022-08-10 14:15:51 +0200

    fill inn checklist

--- src/features/checklists/utils.ts
+++ src/features/checklists/utils.ts
@@ -4,7 +4,16 @@ import { StorageKey } from "config/storage";
 import { getFromStorage, saveToStorage } from "utils/storage";
 import { Checklist, ChecklistInstance } from "./types";
 
-let checklists: Checklist[] = [];
+let checklists: Checklist[] = parseChecklistsStorage();
+let checklistInstances: ChecklistInstance[] = parseChecklistInstanceStorage();
+
+export function getChecklists() {
+    return checklists;
+}
+
+export function getChecklistInstances() {
+    return checklistInstances;
+}
 
 export function addChecklist(partialList: Omit<Checklist, "id">): [Checklist, Checklist[]] {
     const checklist = { ...partialList, id: uuidv4() };
@@ -29,19 +38,16 @@ export function updateChecklist(id: string, updates: Partial<Omit<Checklist, "id
     return checklists;
 }
 
-export function parseChecklistsStorage(): Checklist[] {
+function parseChecklistsStorage(): Checklist[] {
     try {
         const str = getFromStorage(StorageKey.Checklists);
 
-        checklists = str ? JSON.parse(str) : [];
-        return checklists;
+        return str ? JSON.parse(str) : [];
     } catch (e) {
         console.warn(e);
         return [];
     }
 }
-
-let checklistInstances: ChecklistInstance[] = [];
 
 export function addChecklistInstances(partialInstances: Omit<ChecklistInstance, "id">[]): ChecklistInstance[] {
     checklistInstances = checklistInstances.concat(partialInstances.map((inst) => ({ ...inst, id: uuidv4() })));
@@ -64,17 +70,46 @@ export function updateChecklistInstance(
     checklistInstances = checklistInstances.map((cl) => (id === cl.id ? { ...cl, ...updates } : cl));
     saveToStorage(StorageKey.ChecklistInstances, JSON.stringify(checklistInstances));
 
+    const checklistId = checklistInstances.find((cl) => cl.id === id)?.checklistId;
+
+    if (checklistId) {
+        updateChecklistStatus(checklistId);
+    }
+
     return checklistInstances;
 }
 
-export function parseChecklistInstanceStorage(): ChecklistInstance[] {
+function parseChecklistInstanceStorage(): ChecklistInstance[] {
     try {
         const str = getFromStorage(StorageKey.ChecklistInstances);
 
-        checklistInstances = str ? JSON.parse(str) : [];
-        return checklistInstances;
+        return str ? JSON.parse(str) : [];
     } catch (e) {
         console.warn(e);
         return [];
     }
+}
+
+function updateChecklistStatus(id: string) {
+    const checklist = checklists.find((cl) => cl.id === id);
+
+    if (!checklist) {
+        return;
+    }
+
+    const instances = checklistInstances.filter((instance) => instance.checklistId === id);
+    const requiredItems = checklist.items.filter((item) => item.required || item.relevant);
+
+    updateChecklist(id, {
+        instances: {
+            ...checklist.instances,
+            count: instances.length,
+            completed: instances.filter(
+                (instance) =>
+                    !requiredItems.some(
+                        (requiredItem) => !instance.items.find((item) => item.id === requiredItem.id)?.value
+                    )
+            ).length,
+        },
+    });
 }
