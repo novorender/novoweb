efff7d414e2d39064cc2b47edcf5dc13504d4bd5
Ola Folkvord <ola.folki@gmail.com>
2023-05-04 13:14:47 +0200

    case insensitive file extension search

--- src/utils/objectData.ts
+++ src/utils/objectData.ts
@@ -29,7 +29,7 @@ export function getObjectNameFromPath(path: string): string {
 export function getFilePathFromObjectPath(objectPath: string): string | null {
     //https://novorender.com/formats-integrations/
     const match = objectPath.match(
-        /^(?<path>.+\.(dem|dwg|dxf|ifc|xml|kof|nwd|obj|pdms|rvm|step|stp|wms|wmts|pts|las|e57|jpg|jpeg|tif|tiff|pdf))/
+        /^(?<path>.+\.(dem|dwg|dxf|ifc|xml|kof|nwd|obj|pdms|rvm|step|stp|wms|wmts|pts|las|e57|jpg|jpeg|tif|tiff|pdf))/i
     )?.groups;
 
     if (!match || !match.path) {
