4e145cfeff04035d22c54fe4719e623f18f86f72
Sigve Bergslien <sigvebergslien@hotmail.com>
2023-10-02 13:05:19 +0200

    Revert "Outline highlights demo"
    This reverts commit 09d848607228ea42390989e33e6ed8def48bb763.

--- src/features/clippingPlanes/clippingPlanes.tsx
+++ src/features/clippingPlanes/clippingPlanes.tsx
@@ -1,33 +1,20 @@
-import { DeleteSweep, SyncAlt } from "@mui/icons-material";
+import { DeleteSweep } from "@mui/icons-material";
 import { Box, Button, FormControlLabel, Slider } from "@mui/material";
 import { vec4 } from "gl-matrix";
-import { SyntheticEvent, useCallback, useEffect, useRef, useState } from "react";
+import { SyntheticEvent, useEffect, useRef, useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "app/store";
-import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
-    IosSwitch,
-    LogoSpeedDial,
-    ScrollBox,
-    WidgetContainer,
-    WidgetHeader,
-} from "components";
+import { IosSwitch, LogoSpeedDial, ScrollBox, WidgetContainer, WidgetHeader } from "components";
 import { featuresConfig } from "config/features";
 import { useExplorerGlobals } from "contexts/explorerGlobals";
 import { Picker, renderActions, selectClippingPlanes, selectPicker } from "features/render/renderSlice";
 import WidgetList from "features/widgetList/widgetList";
 import { useToggle } from "hooks/useToggle";
 import { selectMaximized, selectMinimized } from "slices/explorerSlice";
-import { getFilePathFromObjectPath } from "utils/objectData";
-import { ClippedFile, ClippedObject } from "./clippedObject";
-import { VecRGB } from "utils/color";
-import { iterateAsync } from "utils/search";
 
 export default function ClippingPlanes() {
     const {
-        state: { view, db },
+        state: { view },
     } = useExplorerGlobals(true);
     const [menuOpen, toggleMenu] = useToggle();
     const minimized = useAppSelector(selectMinimized) === featuresConfig.clippingPlanes.key;
@@ -37,8 +24,6 @@ export default function ClippingPlanes() {
     const dispatch = useAppDispatch();
     const [sliders, setSliders] = useState([] as number[]);
     const isInitial = useRef(true);
-
-    const [clippedFiles, setClippedFiles] = useState([] as ClippedFile[]);
 
     useEffect(() => {
         if (isInitial.current) {
@@ -84,66 +69,6 @@ export default function ClippingPlanes() {
             clipping: { planes: planes.map((p, i) => (i === idx ? { ...selected, normalOffset: plane } : p)) },
         });
     };
-
-    const updateClippedFiles = useCallback(async () => {
-        const getFileId = async (fileName: string) => {
-            const iterator = db.search({ parentPath: fileName }, undefined);
-            const [nodes] = await iterateAsync({ iterator, count: 100000 });
-            const ids: number[] = [];
-            for (const node of nodes) {
-                ids.push(node.id);
-            }
-            return ids;
-        };
-
-        const objIds = await view.getOutlineObjectsOnScreen();
-        if (objIds) {
-            const filePaths = new Set<string>();
-            for (const obj of objIds) {
-                const data = await db.getObjectMetdata(obj);
-                const f = getFilePathFromObjectPath(data.path);
-                if (f) {
-                    filePaths.add(f);
-                }
-            }
-            const files: ClippedFile[] = [];
-
-            function hsl2rgb(h: number, s: number, l: number) {
-                let a = s * Math.min(l, 1 - l);
-                let f = (n: number, k = (n + h / 30) % 12) => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-                console.log(h, s, l);
-                return [f(0), f(8), f(4)];
-            }
-            let i = 0;
-            const increments = 360 / filePaths.size;
-            for (const f of filePaths) {
-                const ids = await getFileId(f);
-                files.push({ name: f, color: hsl2rgb(increments * i, 1, 0.5) as VecRGB, hidden: false, ids });
-                ++i;
-            }
-            view.modifyRenderState({
-                highlights: {
-                    groups: files.map((f) => {
-                        return {
-                            objectIds: f.ids,
-                            outlineColor: f.color,
-                        };
-                    }),
-                },
-            });
-            setClippedFiles(files);
-        }
-    }, [view, db]);
-
-    // useEffect(() => {
-    //     renderFnRef.current = updateClippedObjects;
-    //     return () => (renderFnRef.current = undefined);
-    //     function updateClippedObjects(moved: boolean, idleFrame: boolean) {
-    //         if (view && idleFrame && planes.length > 0) {
-    //             updateClippedFiles();
-    //         }
-    //     }
-    // }, [view, renderFnRef, planes, updateClippedFiles]);
 
     const handleSliderChangeCommitted =
         (idx: number) => (_event: Event | SyntheticEvent<Element, Event>, newValue: number | number[]) => {
@@ -221,22 +146,6 @@ export default function ClippingPlanes() {
                                 </Box>
                             );
                         })}
-
-                    <Accordion>
-                        <AccordionSummary>Clipped objects</AccordionSummary>
-                        <AccordionDetails>
-                            <Box p={1} display="flex" alignItems="start" flexDirection="column">
-                                <Button onClick={() => updateClippedFiles()} color="grey">
-                                    <SyncAlt sx={{ mr: 1 }} />
-                                    Update
-                                </Button>
-
-                                {clippedFiles.map((f, idx) => {
-                                    return <ClippedObject file={f} key={idx} />;
-                                })}
-                            </Box>
-                        </AccordionDetails>
-                    </Accordion>
                 </ScrollBox>
                 {menuOpen && <WidgetList widgetKey={featuresConfig.clippingPlanes.key} onSelect={toggleMenu} />}
             </WidgetContainer>
