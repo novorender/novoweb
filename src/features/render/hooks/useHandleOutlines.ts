3085ce5e0f5f3725600167912f1cd75d33ee5aae
Ola Folkvord <ola.folki@gmail.com>
2023-11-07 13:56:26 +0100

    offline outline colors

--- src/features/render/hooks/useHandleOutlines.ts
+++ src/features/render/hooks/useHandleOutlines.ts
@@ -13,7 +13,8 @@ import {
 import { ClippedFile } from "features/clippingOutline/clippedObject";
 import { VecRGB } from "utils/color";
 import { getFilePathFromObjectPath } from "utils/objectData";
-import { searchByPatterns } from "utils/search";
+import { getObjectData, searchByPatterns } from "utils/search";
+import { sleep } from "utils/time";
 
 import { CameraType, selectCamera, selectClippingPlanes } from "../renderSlice";
 
@@ -43,12 +44,21 @@ export function useHandleOutlines() {
     }, [outlineGroups]);
 
     useEffect(() => {
+        if (!planes.length) {
+            groupRef.current = [];
+        }
+    }, [planes]);
+
+    useEffect(() => {
         updateClippedFiles();
         async function updateClippedFiles() {
             //This function will not work unless the pick buffer is ready. As this can be called from bookmark a long sleep is required for everything to be set up
-            const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
             await sleep(1000);
-            if (db && view && groupRef.current.length === 0) {
+            if (!db || !view || groupRef.current.length !== 0) {
+                return;
+            }
+
+            if (navigator.onLine) {
                 const getFileId = async (fileName: string) => {
                     const iterator = db.search({ parentPath: fileName, descentDepth: 0 }, undefined);
                     const fileId = (await iterator.next()).value;
@@ -73,11 +83,6 @@ export function useHandleOutlines() {
                     });
                     const files: ClippedFile[] = [];
 
-                    function hsl2rgb(h: number, s: number, l: number) {
-                        let a = s * Math.min(l, 1 - l);
-                        let f = (n: number, k = (n + h / 30) % 12) => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-                        return [f(0), f(8), f(4)];
-                    }
                     let i = 0;
                     const increments = 360 / filePaths.size;
                     for (const f of filePaths) {
@@ -87,6 +92,51 @@ export function useHandleOutlines() {
                     }
                     dispatch(clippingOutlineActions.setOutlineGroups(files));
                 }
+            } else {
+                const objIds = await view.getOutlineObjectsOnScreen();
+                if (!objIds) {
+                    return;
+                }
+
+                const files = (
+                    await Promise.all(
+                        Array.from(objIds).map(async (id) => {
+                            const data = await getObjectData({ db, view, id });
+                            if (!data) {
+                                return;
+                            }
+
+                            return { id, path: data.path };
+                        })
+                    )
+                )
+                    .filter((obj): obj is NonNullable<typeof obj> => obj !== undefined)
+                    .reduce((prev, val) => {
+                        const file = getFilePathFromObjectPath(val.path);
+
+                        if (!file) {
+                            return prev;
+                        }
+
+                        if (!prev[file]) {
+                            prev[file] = [val.id];
+                        } else {
+                            prev[file].push(val.id);
+                        }
+
+                        return prev;
+                    }, {} as Record<string, number[]>);
+
+                dispatch(
+                    clippingOutlineActions.setOutlineGroups(
+                        Object.keys(files).map((key, idx, arr) => ({
+                            name: key,
+                            color: hsl2rgb((360 / arr.length) * idx, 1, 0.5),
+                            hidden: false,
+                            ids: files[key],
+                        }))
+                    )
+                );
             }
         }
     }, [db, view, dispatch, planes]);
@@ -176,4 +226,10 @@ export function useHandleOutlines() {
             }
         }
     }, [planes, view, cameraState, dispatch]);
+}
+
+function hsl2rgb(h: number, s: number, l: number): VecRGB {
+    let a = s * Math.min(l, 1 - l);
+    let f = (n: number, k = (n + h / 30) % 12) => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return [f(0), f(8), f(4)];
 }
