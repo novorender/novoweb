bbdf72518b6f29305b5c075d70a2338591278411
Sigve Bergslien <sigvebergslien@hotmail.com>
2023-11-09 12:46:55 +0100

    Line segment lables on ortho

--- src/features/engine2D/engine2D.tsx
+++ src/features/engine2D/engine2D.tsx
@@ -179,7 +179,7 @@ export function Engine2D({
                         measureObjects.map((obj) =>
                             getDrawMeasureEntity(obj, {
                                 ...obj.settings,
-                                segmentLabelInterval: cameraType === CameraType.Pinhole ? 10 : 0,
+                                segmentLabelInterval: 10,
                             })
                         )
                     ),
@@ -187,7 +187,7 @@ export function Engine2D({
                         (drawSelectedPaths ? pathMeasureObjectsData ?? [] : []).map((obj) =>
                             getDrawMeasureEntity(obj, {
                                 cylinderMeasure: drawPathSettings,
-                                segmentLabelInterval: cameraType === CameraType.Pinhole ? 10 : 0,
+                                segmentLabelInterval: 10,
                             })
                         )
                     ),
