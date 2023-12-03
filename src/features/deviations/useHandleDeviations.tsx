8e13fdece5f3772ded0e9b5c2db798ddcbf76ff8
Sigve Bergslien <sigvebergslien@hotmail.com>
2023-12-03 21:02:46 +0100

    Force to first deviation if only one exist

--- src/features/deviations/useHandleDeviations.tsx
+++ src/features/deviations/useHandleDeviations.tsx
@@ -3,12 +3,16 @@ import { useEffect } from "react";
 import { useAppSelector } from "app/store";
 import { useExplorerGlobals } from "contexts/explorerGlobals";
 import { selectDeviations } from "features/render";
+import { AsyncStatus, hasFinished } from "types/misc";
+
+import { selectDeviationProfiles } from "./deviationsSlice";
 
 export function useHandleDeviations() {
     const {
         state: { view },
     } = useExplorerGlobals();
     const deviation = useAppSelector(selectDeviations);
+    const profiles = useAppSelector(selectDeviationProfiles);
 
     useEffect(
         function handleDeviationChanges() {
@@ -16,8 +20,15 @@ export function useHandleDeviations() {
                 return;
             }
 
-            view.modifyRenderState({ points: { deviation } });
+            view.modifyRenderState({
+                points: {
+                    deviation:
+                        hasFinished(profiles) && profiles.status !== AsyncStatus.Error && profiles.data.length < 2
+                            ? { ...deviation, index: 0 }
+                            : deviation,
+                },
+            });
         },
-        [view, deviation]
+        [view, deviation, profiles]
     );
 }
