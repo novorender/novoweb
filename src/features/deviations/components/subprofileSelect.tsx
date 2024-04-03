c1c0b08343f0d323153c31eaa57f8035a2dc27e1
nd-novorender <nd@novorender.com>
2024-04-03 10:32:34 +0200

    When switching subprofile in cross section view - switch all corresponding follow path settings

--- src/features/deviations/components/subprofileSelect.tsx
+++ src/features/deviations/components/subprofileSelect.tsx
@@ -1,21 +1,38 @@
-import { Box, FormControl, FormHelperText, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import {
+    Box,
+    FormControl,
+    FormHelperText,
+    InputLabel,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+    Typography,
+} from "@mui/material";
 import { useMemo } from "react";
 
 import { useAppDispatch, useAppSelector } from "app/redux-store-interactions";
+import { useExplorerGlobals } from "contexts/explorerGlobals";
 import { useObjectGroups } from "contexts/objectGroups";
-import { selectLandXmlPaths } from "features/followPath";
+import { selectLandXmlPaths, selectProfile, selectView2d } from "features/followPath";
 import { useLoadLandXmlPath } from "features/followPath/hooks/useLoadLandXmlPath";
+import { useGoToProfile } from "features/followPath/useGoToProfile";
 import { AsyncStatus } from "types/misc";
 
 import { deviationsActions, selectSelectedProfile, selectSelectedSubprofileIndex } from "../deviationsSlice";
 import { DELETED_DEVIATION_LABEL } from "../utils";
 
 export function SubprofileSelect() {
+    const {
+        state: { view },
+    } = useExplorerGlobals(true);
     const dispatch = useAppDispatch();
     const landXmlPaths = useAppSelector(selectLandXmlPaths);
     const selectedProfile = useAppSelector(selectSelectedProfile);
     const selectedSubprofileIndex = useAppSelector(selectSelectedSubprofileIndex);
     const objectGroups = useObjectGroups();
+    const view2d = useAppSelector(selectView2d);
+    const profilePos = useAppSelector(selectProfile);
+    const goToProfile = useGoToProfile();
 
     useLoadLandXmlPath();
 
@@ -61,6 +78,37 @@ export function SubprofileSelect() {
         return null;
     }
 
+    const handleChange = async (e: SelectChangeEvent<number>) => {
+        if (!selectedProfile) {
+            return;
+        }
+
+        const spIndex = Number(e.target.value);
+        const sp = selectedProfile.subprofiles[spIndex];
+
+        dispatch(deviationsActions.setSelectedSubprofileIndex(spIndex));
+
+        if (view2d && sp.centerLine) {
+            const fpObj = await view.measure?.followPath.followParametricObjects([sp.centerLine.objectId], {
+                cylinderMeasure: "center",
+            });
+
+            if (fpObj) {
+                const pos = Math.max(
+                    sp.centerLine.parameterBounds[0],
+                    Math.min(sp.centerLine.parameterBounds[1], Number(profilePos))
+                );
+
+                goToProfile({
+                    fpObj: fpObj,
+                    p: pos,
+                    newView2d: true,
+                    keepOffset: false,
+                });
+            }
+        }
+    };
+
     return (
         <FormControl fullWidth>
             <InputLabel id="select-center-line-label">Select subprofile</InputLabel>
@@ -69,9 +117,7 @@ export function SubprofileSelect() {
                 id="select-center-line"
                 value={selectedSubprofileIndex ?? ""}
                 label="Select subprofile"
-                onChange={(e) => {
-                    dispatch(deviationsActions.setSelectedSubprofileIndex(Number(e.target.value)));
-                }}
+                onChange={handleChange}
             >
                 {options.map((option) => (
                     <MenuItem key={option.index} value={option.index}>
