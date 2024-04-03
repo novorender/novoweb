8efb0f4f9214e320836b7cdd7cea338c9c3de6c6
Ola Folkvord <ola.folki@gmail.com>
2024-04-03 15:38:51 +0200

    update svg marker positions similar to engine2d html interactions
    Squashed commit of the following:
    
    commit f9424ce41a94bef3a7fd6cecde043b9df5e3b602
    Author: Ola Folkvord <ola.folki@gmail.com>
    Date:   Wed Apr 3 15:35:01 2024 +0200
    
        jira markers
    
    commit 8148d436df565de179d72d827d007ffcf34909a9
    Author: Ola Folkvord <ola.folki@gmail.com>
    Date:   Wed Apr 3 13:54:38 2024 +0200
    
        xsite markers
    
    commit 78e349b3a09198839bc64da8af3583273d92491b
    Author: Ola Folkvord <ola.folki@gmail.com>
    Date:   Wed Apr 3 13:23:46 2024 +0200
    
        image markers
    
    commit 110403f1b07ce26010c392e92eba301647d659fc
    Author: Ola Folkvord <ola.folki@gmail.com>
    Date:   Wed Apr 3 12:42:19 2024 +0200
    
        ditio machine markers
    
    commit a7a81c1ec260e348b370fb12b3afb52eea6c6215
    Author: Ola Folkvord <ola.folki@gmail.com>
    Date:   Wed Apr 3 10:57:52 2024 +0200
    
        ditio feed markers
    
    commit 77b6143925bb1ec83f993ca72990eccb21a71046
    Author: Ola Folkvord <ola.folki@gmail.com>
    Date:   Tue Apr 2 16:45:14 2024 +0200
    
        mylocation

--- src/features/render/markers.tsx
+++ src/features/render/markers.tsx
@@ -1,19 +1,39 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
 import { DitioFeedMarkers, DitioMachineMarkers } from "features/ditio";
 import { ImageMarkers } from "features/images";
 import { JiraMarkers } from "features/jira";
 import { MyLocationMarker } from "features/myLocation";
 import { LogPointMarkers, MachineLocationMarkers } from "features/xsiteManage";
 
-export function Markers() {
+export const Markers = forwardRef(function Markers(_, ref) {
+    const childRefs = useRef([] as ({ update: () => void } | null)[]);
+
+    useImperativeHandle(
+        ref,
+        () => ({
+            update() {
+                for (const childRef of childRefs.current) {
+                    childRef?.update();
+                }
+            },
+        }),
+        []
+    );
+
     return (
         <>
-            <MyLocationMarker />
-            <DitioFeedMarkers />
-            <DitioMachineMarkers />
-            <ImageMarkers />
-            <MachineLocationMarkers />
-            <LogPointMarkers />
-            <JiraMarkers />
+            {[
+                MyLocationMarker,
+                DitioFeedMarkers,
+                DitioMachineMarkers,
+                ImageMarkers,
+                MachineLocationMarkers,
+                LogPointMarkers,
+                JiraMarkers,
+            ].map((Container, idx) => (
+                <Container key={idx} ref={(e) => (childRefs.current[idx] = e)} />
+            ))}
         </>
     );
-}
+});
