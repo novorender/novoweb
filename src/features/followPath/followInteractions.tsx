fcc34fcbda7e01265ddb35fe652146d93c741b01
nd-novorender <nd@novorender.com>
2024-05-24 12:54:50 +0200

    Follow path: change icon for close button

--- src/features/followPath/followInteractions.tsx
+++ src/features/followPath/followInteractions.tsx
@@ -75,16 +75,22 @@ const InfoMarker = styled(
     { shouldForwardProp: (prop) => prop !== "active" && prop !== "hovered" }
 )(markerStyles);
 
+const basicStyle = () => css`
+    cursor: pointer;
+    pointer-events: bounding-box;
+    filter: drop-shadow(3px 3px 2px rgba(0, 0, 0, 0.3));
+`;
+
 const CloseMarker = styled(
     (props: SVGProps<SVGGElement>) => (
         <g {...props}>
             <rect width="24" height="24" fillOpacity={0} transform={"translate(88 88)"} />
-            <circle r="12" transform={"translate(100 100)"} />
-            <path d="M6 18 18 6M6 6l12 12" strokeWidth={2} transform={"translate(88 88)"}></path>
+            <circle r="10" fill="red" transform={"translate(100 100)"} />
+            <path d="M96,96 L104,104 M104,96 L96,104" stroke="white" strokeWidth={2}></path>
         </g>
     ),
     { shouldForwardProp: (prop) => prop !== "active" && prop !== "hovered" }
-)(markerStyles);
+)(basicStyle);
 
 export function FollowInteractions() {
     const goToProfile = useGoToProfile();
