53ed71c5a6a2d1125c5bcb517f4ade0a6a7d753d
nd-novorender <158474630+nd-novorender@users.noreply.github.com>
2024-06-26 08:45:21 +0200

    Fly to: stay in ortho when in 2d mode (#89)
    Fly to: stay in ortho when in 2d mode

--- src/features/flyToSelected/flyToSelected.tsx
+++ src/features/flyToSelected/flyToSelected.tsx
@@ -1,22 +1,32 @@
-import { Box, CircularProgress, SpeedDialActionProps } from "@mui/material";
+import { Box, CircularProgress, SpeedDialActionProps, Tooltip } from "@mui/material";
+import { View } from "@novorender/api";
+import { AABB2 } from "@novorender/api/types/measure/worker/brep";
 import { BoundingSphere } from "@novorender/webgl-api";
-import { vec3 } from "gl-matrix";
+import { glMatrix, vec2, vec3 } from "gl-matrix";
 import { useEffect, useRef, useState } from "react";
 
-import { useAppDispatch } from "app/redux-store-interactions";
+import { useAppDispatch, useAppSelector } from "app/redux-store-interactions";
 import { SpeedDialAction } from "components";
 import { featuresConfig } from "config/features";
 import { useExplorerGlobals } from "contexts/explorerGlobals";
 import { useHighlighted } from "contexts/highlighted";
+import { getCameraDir } from "features/engine2D/utils";
 import { imagesActions } from "features/images";
-import { CameraType, renderActions } from "features/render";
+import { CameraType, renderActions, selectCameraType, selectViewMode } from "features/render";
 import { useAbortController } from "hooks/useAbortController";
+import { ViewMode } from "types/misc";
+import { isRectInsideCircle, pointToPlaneDistance, pointToRectDistance } from "utils/math";
 import { objIdsToTotalBoundingSphere } from "utils/objectData";
+import { sleep } from "utils/time";
 
 enum Status {
     Initial,
     Loading,
 }
+
+// Padding to add around object when flying to it in orthographic view
+// 0.2 is 20% of bounding sphere diameter (or bounding rect max dimension)
+const ORTHO_PADDING = 0.2;
 
 type Props = SpeedDialActionProps & {
     position?: { top?: number; right?: number; bottom?: number; left?: number };
@@ -29,11 +39,29 @@ export function FlyToSelected({ position, ...speedDialProps }: Props) {
         state: { db, scene, view },
     } = useExplorerGlobals(true);
     const dispatch = useAppDispatch();
+    const cameraType = useAppSelector(selectCameraType);
+    const viewMode = useAppSelector(selectViewMode);
+    const isCrossSection = [ViewMode.CrossSection, ViewMode.FollowPath, ViewMode.Deviations].includes(viewMode);
 
     const [status, setStatus] = useState(Status.Initial);
 
     const previousBoundingSphere = useRef<BoundingSphere>();
     const [abortController, abort] = useAbortController();
+
+    const [tooltipMessage, setTooltipMessage] = useState("");
+    const tooltipTimeout = useRef<ReturnType<typeof setTimeout>>();
+    const orthoLoadingHandle = useRef(0);
+    const [orthoLoading, setOrthoLoading] = useState(false);
+
+    const showTooltip = (msg: string) => {
+        if (tooltipTimeout.current) {
+            clearTimeout(tooltipTimeout.current);
+        }
+        setTooltipMessage(msg);
+        tooltipTimeout.current = setTimeout(() => {
+            setTooltipMessage("");
+        }, 3000);
+    };
 
     useEffect(() => {
         previousBoundingSphere.current = undefined;
@@ -42,13 +70,128 @@ export function FlyToSelected({ position, ...speedDialProps }: Props) {
     }, [highlighted, abort, setStatus]);
 
     const handleClick = async () => {
-        if (!highlighted.length) {
+        if (!highlighted.length || !view) {
             return;
         }
 
-        if (previousBoundingSphere.current) {
-            dispatch(renderActions.setCamera({ type: CameraType.Pinhole, zoomTo: previousBoundingSphere.current }));
+        if (orthoLoadingHandle.current) {
+            setOrthoLoading(false);
+            orthoLoadingHandle.current = 0;
+        }
+
+        const lookAtSphereInOrtho = (sphere: BoundingSphere) => {
+            const cameraDir = getCameraDir(view.renderState.camera.rotation);
+            const pinholeFov = view.controllers.flight.fov;
+            const dist = Math.max(sphere.radius / Math.tan(glMatrix.toRadian(pinholeFov) / 2), sphere.radius);
+            const offset = -Math.min(dist, view.renderState.camera.far * 0.9);
+            const position = vec3.scaleAndAdd(vec3.create(), sphere.center, cameraDir, offset);
+
+            dispatch(
+                renderActions.setCamera({
+                    type: CameraType.Orthographic,
+                    goTo: {
+                        position,
+                        rotation: view.renderState.camera.rotation,
+                        fov: sphere.radius * 2 * (1 + ORTHO_PADDING),
+                        far: view.renderState.camera.far,
+                    },
+                })
+            );
+        };
+
+        const goToOrtho = async (sphere: BoundingSphere) => {
+            if (isCrossSection && view.renderState.clipping.planes.length > 0) {
+                // In cross section bounding sphere might differ from the visible object part a lot,
+                // so we try to focus on the outline instead of bounding sphere.
+                // If object far outside the view (based on the bounding sphere) - focus bounding sphere first
+                // and then after 1 second (so outlines are generated) - focus on the outlines.
+                const plane = view.renderState.clipping.planes[0];
+                const planeToSphereDist = pointToPlaneDistance(sphere.center, plane.normalOffset);
+                if (planeToSphereDist > sphere.radius + view.renderState.camera.far) {
+                    showTooltip("Object is outside the current cross section view");
+                    return;
+                }
+
+                const width = window.innerWidth;
+                const height = window.innerHeight;
+                const screenRect = {
+                    min: vec2.fromValues(0, 0),
+                    max: vec2.fromValues(width, height),
+                };
+
+                const [sphereCenter2d] = view.convert
+                    .worldSpaceToViewSpace([sphere.center])
+                    .map((p) => vec2.fromValues(p[0] * width, p[1] * height));
+
+                let visibleRadiusRatio = 0;
+                if (sphereCenter2d) {
+                    const sphereRadius2d = (height / view.renderState.camera.fov) * sphere.radius;
+                    const isSphereCenterInsideScreenRect =
+                        sphereCenter2d[0] >= 0 &&
+                        sphereCenter2d[0] <= width &&
+                        sphereCenter2d[1] >= 0 &&
+                        sphereCenter2d[1] <= height;
+
+                    if (
+                        isSphereCenterInsideScreenRect ||
+                        isRectInsideCircle(screenRect, sphereCenter2d, sphereRadius2d)
+                    ) {
+                        visibleRadiusRatio = 1;
+                    } else {
+                        const sphereToRectDist = pointToRectDistance(sphereCenter2d, screenRect);
+                        visibleRadiusRatio = Math.max(0, (sphereRadius2d - sphereToRectDist) / sphereRadius2d);
+                    }
+                }
+
+                let loading = 0;
+                if (visibleRadiusRatio < 0.5) {
+                    orthoLoadingHandle.current = loading = performance.now();
+                    setOrthoLoading(true);
+                    lookAtSphereInOrtho(sphere);
+
+                    // Wait for camera to move and outlines to be generated
+                    await sleep(1000);
+                }
+
+                if (loading) {
+                    if (loading !== orthoLoadingHandle.current) {
+                        return;
+                    } else {
+                        orthoLoadingHandle.current = 0;
+                        setOrthoLoading(false);
+                    }
+                }
+
+                const zoomingParams = getSelectedObjectsOutlineZoomingParams(view, new Set(highlighted));
+                if (zoomingParams) {
+                    dispatch(
+                        renderActions.setCamera({
+                            type: CameraType.Orthographic,
+                            goTo: {
+                                position: zoomingParams.position,
+                                rotation: view.renderState.camera.rotation,
+                                fov: zoomingParams.fov * (1 + ORTHO_PADDING),
+                                far: view.renderState.camera.far,
+                            },
+                        })
+                    );
+                }
+            } else {
+                lookAtSphereInOrtho(sphere);
+            }
+        };
+
+        const goTo = (sphere: BoundingSphere) => {
+            if (cameraType === CameraType.Pinhole) {
+                dispatch(renderActions.setCamera({ type: CameraType.Pinhole, zoomTo: sphere }));
+            } else {
+                goToOrtho(sphere);
+            }
             dispatch(imagesActions.setActiveImage(undefined));
+        };
+
+        if (previousBoundingSphere.current) {
+            goTo(previousBoundingSphere.current);
             return;
         }
 
@@ -66,8 +209,7 @@ export function FlyToSelected({ position, ...speedDialProps }: Props) {
 
             if (boundingSphere) {
                 previousBoundingSphere.current = boundingSphere;
-                dispatch(renderActions.setCamera({ type: CameraType.Pinhole, zoomTo: boundingSphere }));
-                dispatch(imagesActions.setActiveImage(undefined));
+                goTo(boundingSphere);
             }
         } finally {
             setStatus(Status.Initial);
@@ -87,20 +229,56 @@ export function FlyToSelected({ position, ...speedDialProps }: Props) {
             onClick={handleClick}
             title={disabled ? undefined : name}
             icon={
-                <Box
-                    width={1}
-                    height={1}
-                    position="relative"
-                    display="inline-flex"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    {status === Status.Loading ? (
-                        <CircularProgress thickness={2.5} sx={{ position: "absolute" }} />
-                    ) : null}
-                    <Icon />
-                </Box>
+                <Tooltip open={Boolean(tooltipMessage)} title={tooltipMessage} placement="top">
+                    <Box
+                        width={1}
+                        height={1}
+                        position="relative"
+                        display="inline-flex"
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+                        {status === Status.Loading || orthoLoading ? (
+                            <CircularProgress thickness={2.5} sx={{ position: "absolute" }} />
+                        ) : null}
+                        <Icon />
+                    </Box>
+                </Tooltip>
             }
         />
     );
+}
+
+function getSelectedObjectsOutlineZoomingParams(view: View, objectIds: Set<number>) {
+    const box = view.getObjectsOutlinePlaneBoundingRectInWorld("clipping", 0, objectIds);
+    if (!box) {
+        return;
+    }
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    const center3d = vec3.lerp(vec3.create(), box.min, box.max, 0.5);
+    const [screenMin, screenMax] = view.convert
+        .worldSpaceToViewSpace([box.min, box.max])
+        .map((p) => vec2.fromValues(p[0] * width, p[1] * height));
+
+    const fovK = getOrthoFovRatioForAABB2(width, height, { min: screenMin, max: screenMax });
+    if (!fovK) {
+        return;
+    }
+
+    return {
+        position: center3d,
+        fov: view.renderState.camera.fov * fovK,
+    };
+}
+
+function getOrthoFovRatioForAABB2(screenWidth: number, screenHeight: number, bbox: AABB2): number {
+    const width = Math.abs(bbox.max[0] - bbox.min[0]);
+    const height = Math.abs(bbox.max[1] - bbox.min[1]);
+    const kHeight = screenHeight / height;
+    const kWidth = screenWidth / width;
+    const k = Math.min(kHeight, kWidth);
+    return 1 / k;
 }
