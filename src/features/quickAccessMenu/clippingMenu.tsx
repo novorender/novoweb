e02a47755929256f9f00514fb355be82af1e7b1e
nd-novorender <nd@novorender.com>
2024-08-08 13:49:54 +0200

    Quick access measure and clipping menus turn on tools instead of showing widgets

--- src/features/quickAccessMenu/clippingMenu.tsx
+++ src/features/quickAccessMenu/clippingMenu.tsx
@@ -1,11 +1,73 @@
-import { MenuList } from "@mui/material";
+import { ListItemIcon, ListItemText, MenuItem, MenuList, useTheme } from "@mui/material";
 
-import { WidgetMenuItem } from "./widgetMenuItem";
+import { useAppDispatch, useAppSelector } from "app/redux-store-interactions";
+import { featuresConfig } from "config/features";
+import { Picker, renderActions, selectPicker } from "features/render";
 
 export function ClippingMenu({ onSelect }: { onSelect?: () => void }) {
+    const activePicker = useAppSelector(selectPicker);
+    // const cameraType = useAppSelector(selectCameraType);
+    // const selectingOrthoPoint = activePicker === Picker.OrthoPlane;
+    const dispatch = useAppDispatch();
+
+    const togglePicker = (picker: Picker) => {
+        dispatch(renderActions.setPicker(picker === activePicker ? Picker.Object : picker));
+        onSelect?.();
+    };
+
     return (
         <MenuList>
-            <WidgetMenuItem feature="clippingPlanes" onSelect={onSelect} />
+            <MenuItem onClick={() => togglePicker(Picker.ClippingPlane)}>
+                <ListItemIcon>
+                    <ActiveIcon
+                        Icon={featuresConfig.clippingPlanes.Icon}
+                        active={activePicker === Picker.ClippingPlane}
+                    />
+                </ListItemIcon>
+                <ListItemText>Clipping plane</ListItemText>
+            </MenuItem>
+            {/* <MenuItem
+                onClick={() => {
+                    if (cameraType === CameraType.Orthographic || selectingOrthoPoint) {
+                        dispatch(renderActions.setPicker(Picker.Object));
+                        dispatch(renderActions.setCamera({ type: CameraType.Pinhole }));
+                    } else {
+                        dispatch(renderActions.setPicker(Picker.OrthoPlane));
+                    }
+                    onSelect?.();
+                }}
+            >
+                <ListItemIcon>
+                    <ActiveIcon Icon={featuresConfig.orthoCam.Icon} active={activePicker === Picker.OrthoPlane} />
+                </ListItemIcon>
+                <ListItemText>2D mode</ListItemText>
+            </MenuItem> */}
+            {/* <MenuItem
+                onClick={(e) => {
+                    if (activePicker !== Picker.CrossSection) {
+                        dispatch(renderActions.setPicker(Picker.CrossSection));
+                    } else {
+                        dispatch(renderActions.setPicker(Picker.Object));
+                        dispatch(orthoCamActions.setCrossSectionPoint(undefined));
+                        dispatch(orthoCamActions.setCrossSectionHover(undefined));
+                    }
+                    onSelect?.();
+                }}
+            >
+                <ListItemIcon>
+                    <ActiveIcon
+                        Icon={featuresConfig.clippingPlanes.Icon}
+                        active={activePicker === Picker.CrossSection}
+                    />
+                </ListItemIcon>
+                <ListItemText>Cross section</ListItemText>
+            </MenuItem> */}
         </MenuList>
     );
+}
+
+function ActiveIcon({ Icon, active }: { Icon: typeof featuresConfig.area.Icon; active: boolean }) {
+    const theme = useTheme();
+
+    return <Icon fontSize="small" sx={{ color: active ? theme.palette.primary.main : undefined }} />;
 }
--- src/features/quickAccessMenu/measureMenu.tsx
+++ src/features/quickAccessMenu/measureMenu.tsx
@@ -1,14 +1,56 @@
-import { MenuList } from "@mui/material";
+import { ListItemIcon, ListItemText, MenuItem, useTheme } from "@mui/material";
 
-import { WidgetMenuItem } from "./widgetMenuItem";
+import { useAppDispatch, useAppSelector } from "app/redux-store-interactions";
+import { featuresConfig } from "config/features";
+import { Picker, renderActions, selectPicker } from "features/render";
 
 export function MeasureMenu({ onSelect }: { onSelect?: () => void }) {
+    const activePicker = useAppSelector(selectPicker);
+    const dispatch = useAppDispatch();
+
+    const togglePicker = (picker: Picker) => {
+        dispatch(renderActions.setPicker(picker === activePicker ? Picker.Object : picker));
+        onSelect?.();
+    };
+
     return (
-        <MenuList>
-            <WidgetMenuItem feature="area" onSelect={onSelect} />
-            <WidgetMenuItem feature="outlineLaser" onSelect={onSelect} />
-            <WidgetMenuItem feature="manhole" onSelect={onSelect} />
-            <WidgetMenuItem feature="pointLine" onSelect={onSelect} />
-        </MenuList>
+        <>
+            <MenuItem onClick={() => togglePicker(Picker.Measurement)}>
+                <ListItemIcon>
+                    <ActiveIcon Icon={featuresConfig.measure.Icon} active={activePicker === Picker.Measurement} />
+                </ListItemIcon>
+                <ListItemText>Point to point</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={() => togglePicker(Picker.Area)}>
+                <ListItemIcon>
+                    <ActiveIcon Icon={featuresConfig.area.Icon} active={activePicker === Picker.Area} />
+                </ListItemIcon>
+                <ListItemText>Area</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={() => togglePicker(Picker.OutlineLaser)}>
+                <ListItemIcon>
+                    <ActiveIcon Icon={featuresConfig.outlineLaser.Icon} active={activePicker === Picker.OutlineLaser} />
+                </ListItemIcon>
+                <ListItemText>Laser</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={() => togglePicker(Picker.Manhole)}>
+                <ListItemIcon>
+                    <ActiveIcon Icon={featuresConfig.manhole.Icon} active={activePicker === Picker.Manhole} />
+                </ListItemIcon>
+                <ListItemText>Manhole</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={() => togglePicker(Picker.PointLine)}>
+                <ListItemIcon>
+                    <ActiveIcon Icon={featuresConfig.pointLine.Icon} active={activePicker === Picker.PointLine} />
+                </ListItemIcon>
+                <ListItemText>Point line</ListItemText>
+            </MenuItem>
+        </>
     );
+}
+
+function ActiveIcon({ Icon, active }: { Icon: typeof featuresConfig.area.Icon; active: boolean }) {
+    const theme = useTheme();
+
+    return <Icon fontSize="small" sx={{ color: active ? theme.palette.primary.main : undefined }} />;
 }
