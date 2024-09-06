5f6bc4f7dc45771f2736ece6bc45fe9347877a08
nd-novorender <nd@novorender.com>
2024-09-06 12:32:36 +0200

    Favorites: filter by enabledWidgets

--- src/features/quickAccessMenu/favoritesMenu.tsx
+++ src/features/quickAccessMenu/favoritesMenu.tsx
@@ -1,17 +1,24 @@
 import { StarOutline } from "@mui/icons-material";
 import { Box, MenuList } from "@mui/material";
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
 import { useAppSelector } from "app/redux-store-interactions";
-import { selectFavoriteWidgets } from "slices/explorer";
+import { selectEnabledWidgets, selectFavoriteWidgets } from "slices/explorer";
 
 import { WidgetMenuItem } from "./widgetMenuItem";
 
 export function FavoritesMenu({ onSelect }: { onSelect?: () => void }) {
+    const enabledWidgets = useAppSelector(selectEnabledWidgets);
     const favoriteWidgets = useAppSelector(selectFavoriteWidgets);
     const { t } = useTranslation();
 
-    if (favoriteWidgets.length === 0) {
+    const filteredWidgets = useMemo(
+        () => favoriteWidgets.filter((key) => enabledWidgets.some((w) => w.key === key)),
+        [favoriteWidgets, enabledWidgets],
+    );
+
+    if (filteredWidgets.length === 0) {
         return (
             <Box textAlign="center" m={2} color="grey" sx={{ maxWidth: "240px", verticalAlign: "text-bottom" }}>
                 {t("noFavoriteWidgets1") + " "}
@@ -22,7 +29,7 @@ export function FavoritesMenu({ onSelect }: { onSelect?: () => void }) {
 
     return (
         <MenuList>
-            {favoriteWidgets.map((widget) => (
+            {filteredWidgets.map((widget) => (
                 <WidgetMenuItem key={widget} feature={widget} onSelect={onSelect} />
             ))}
         </MenuList>
