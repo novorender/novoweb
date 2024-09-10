281543c9285f8856a73eb39b0938c83a8323bc16
Bogdan Arabadzhi <8407846+barabadzhi@users.noreply.github.com>
2024-09-10 13:20:20 +0200

    Update minimap
    Signed-off-by: Bogdan Arabadzhi <8407846+barabadzhi@users.noreply.github.com>

--- src/features/minimap/minimap.tsx
+++ src/features/minimap/minimap.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 
 import { useExplorerGlobals } from "contexts/explorerGlobals";
 import { downloadMinimap, MinimapHelper } from "utils/minimap";
+import { getAssetUrl } from "utils/misc";
 
 const Canvas = styled("canvas")`
     background-color: rgba(255, 255, 255, 0.8);
@@ -19,7 +20,7 @@ export function Minimap() {
     } = useExplorerGlobals(true);
 
     let width = Math.min(500, size.width / devicePixelRatio);
-    const height = size.height;
+    const height = size.height / 2;
     const [minimap, setMinimap] = useState<MinimapHelper | undefined>(undefined);
     const [ctx, setCtx] = useState<CanvasRenderingContext2D | null | undefined>(null);
     const animationFrameId = useRef<number>(-1);
@@ -28,14 +29,15 @@ export function Minimap() {
     const prevCamRot = useRef<quat>();
 
     if (minimap) {
-        width = height * minimap.getAspect(); // If aspect changes between pdfs then this needs to be updated
+        width = height * minimap.getAspect();
     }
 
     useEffect(() => {
         const downloadFunc = async () => {
             if (canvas) {
-                setMinimap(await downloadMinimap(db));
-                setCtx(canvas?.getContext("2d"));
+                const minimapHelper = await downloadMinimap(db);
+                setMinimap(minimapHelper);
+                setCtx(canvas.getContext("2d"));
             }
         };
         downloadFunc();
@@ -44,40 +46,46 @@ export function Minimap() {
     useEffect(() => {
         if (minimap && ctx) {
             const img = new Image();
-            img.onload = function () {
-                minimap.pixelHeight = img.height * 1.5; //Set canvas height in minimap helper
-                minimap.pixelWidth = img.width * 1.5; //Set canvas width in minimap helper
-                if (ctx && minimap) {
-                    //ctx.drawImage(img, 450, 200, img.width * 0.7, img.height * 0.7, 0, 0, width, height);
-                    ctx.drawImage(img, 300, 0, img.width, img.height, 0, 0, img.width * 1.5, img.height * 1.5);
-                }
-                //minimap.pixelHeight = height; //Set canvas height in minimap helper
-                //minimap.pixelWidth = width; //Set canvas width in minimap helper
-            };
+            img.crossOrigin = "anonymous";
+            img.onload = () => {
+                minimap.pixelHeight = img.height * 1.5;
+                minimap.pixelWidth = img.width * 1.5;
+                ctx.clearRect(0, 0, width, height);
 
-            img.src = minimap.getMinimapImage();
+                const imgAspectRatio = img.width / img.height;
+                const canvasAspectRatio = width / height;
+
+                let drawWidth, drawHeight;
+                if (imgAspectRatio > canvasAspectRatio) {
+                    drawWidth = width;
+                    drawHeight = width / imgAspectRatio;
+                } else {
+                    drawHeight = height;
+                    drawWidth = height * imgAspectRatio;
+                }
+
+                const offsetX = (width - drawWidth) / 2;
+                const offsetY = (height - drawHeight) / 2;
+
+                ctx.drawImage(img, offsetX, offsetY, drawWidth, drawHeight);
+            };
+            const src = getAssetUrl(view, minimap.getMinimapImage());
+            img.src = src.toString();
         }
-    }, [canvas, width, height, ctx, minimap]);
+    }, [canvas, width, height, ctx, minimap, view]);
 
     const clickMinimap = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
         if (canvas && minimap) {
             const rect = canvas.getBoundingClientRect();
             const x = event.clientX - rect.left;
             const y = event.clientY - rect.top;
-            // view.renderState.camera.controller.moveTo(
-            //     minimap.toWorld(vec2.fromValues(x * (1 / 0.7) + 300, y * (1 / 0.7) + 200)),
-            //     view.renderState.camera.rotation
-            // );
-
-            // view.renderState.camera.controller.moveTo(minimap.toWorld(vec2.fromValues(x + 300 * 1.5, y)), view.renderState.camera.rotation);
             const controller = view.controllers["ortho"];
-            controller.moveTo(minimap.toWorld(vec2.fromValues(x + 450, y)), 1000, view.renderState.camera.rotation);
+            controller.moveTo(minimap.toWorld(vec2.fromValues(x, y)), 1000, view.renderState.camera.rotation);
         }
     };
+
     useEffect(() => {
-        animate();
         function animate() {
-            // Run every frame to check if the camera has changed
             if (
                 !prevCamRot.current ||
                 !quat.exactEquals(prevCamRot.current, view.renderState.camera.rotation) ||
@@ -87,48 +95,56 @@ export function Minimap() {
                 prevCamRot.current = quat.clone(view.renderState.camera.rotation);
                 prevCamPos.current = vec3.clone(view.renderState.camera.position);
                 if (minimap && ctx) {
-                    //Update minimap info based on camera position. Returns true if it changed the pdf to another floor
                     minimap.update(view.renderState.camera.position);
                     const img = new Image();
-                    img.onload = function () {
-                        if (ctx && minimap) {
-                            //Redraw the image for te minimap
-                            ctx.clearRect(0, 0, width / 2, height);
+                    img.crossOrigin = "anonymous";
+                    img.onload = () => {
+                        ctx.clearRect(0, 0, width, height);
 
-                            //ctx.drawImage(img, 450, 200, img.width * 0.7, img.height * 0.7, 0, 0, width, height);
-                            ctx.drawImage(img, 300, 0, img.width, img.height, 0, 0, img.width * 1.5, img.height * 1.5);
+                        const imgAspectRatio = img.width / img.height;
+                        const canvasAspectRatio = width / height;
 
-                            //Gets the camera position in minimap space
-                            const minimapPos = minimap.toMinimap(view.renderState.camera.position);
-                            minimapPos[0] -= 300 * 1.5;
-                            //minimapPos[1] -= 200;
-                            //Gets a cone of the camera direction in minimap space, point[0] is the camera position
-                            const dirPath = minimap.directionPoints(
-                                view.renderState.camera.position,
-                                view.renderState.camera.rotation,
-                            );
-                            ctx.strokeStyle = "green";
-                            for (let i = 1; i < dirPath.length; ++i) {
-                                ctx.beginPath();
-                                ctx.lineWidth = 3;
-                                ctx.moveTo(dirPath[0][0] - 300 * 1.5, dirPath[0][1]);
-                                ctx.lineTo(dirPath[i][0] - 300 * 1.5, dirPath[i][1]);
-                                ctx.stroke();
-                            }
-                            ctx.fillStyle = "green";
-                            ctx.beginPath();
-                            ctx.ellipse(minimapPos[0], minimapPos[1], 5, 5, 0, 0, Math.PI * 2);
-                            ctx.fill();
+                        let drawWidth, drawHeight;
+                        if (imgAspectRatio > canvasAspectRatio) {
+                            drawWidth = width;
+                            drawHeight = width / imgAspectRatio;
+                        } else {
+                            drawHeight = height;
+                            drawWidth = height * imgAspectRatio;
                         }
+
+                        const offsetX = (width - drawWidth) / 2;
+                        const offsetY = (height - drawHeight) / 2;
+
+                        ctx.drawImage(img, offsetX, offsetY, drawWidth, drawHeight);
+
+                        const minimapPos = minimap.toMinimap(view.renderState.camera.position);
+                        const dirPath = minimap.directionPoints(
+                            view.renderState.camera.position,
+                            view.renderState.camera.rotation,
+                        );
+                        ctx.strokeStyle = "green";
+                        ctx.lineWidth = 3;
+                        ctx.beginPath();
+                        ctx.moveTo(dirPath[0][0], dirPath[0][1]);
+                        for (let i = 1; i < dirPath.length; ++i) {
+                            ctx.lineTo(dirPath[i][0], dirPath[i][1]);
+                        }
+                        ctx.stroke();
+                        ctx.fillStyle = "green";
+                        ctx.beginPath();
+                        ctx.ellipse(minimapPos[0], minimapPos[1], 5, 5, 0, 0, Math.PI * 2);
+                        ctx.fill();
                     };
-                    img.src = minimap.getMinimapImage();
+                    const src = getAssetUrl(view, minimap.getMinimapImage());
+                    img.src = src.toString();
                 }
             }
-
-            animationFrameId.current = requestAnimationFrame(() => animate());
+            animationFrameId.current = requestAnimationFrame(animate);
         }
+        animate();
         return () => cancelAnimationFrame(animationFrameId.current);
     }, [view, minimap, ctx, height, width]);
 
-    return <Canvas ref={setCanvas} width={width / 2} height={height} onClick={(e) => clickMinimap(e)} />;
+    return <Canvas ref={setCanvas} width={width} height={height} onClick={clickMinimap} />;
 }
