793260a03c7e5a992b973f0e25e50b6e57689a15
nd-novorender <nd@novorender.com>
2024-09-27 15:47:55 +0200

    Disable classification and intensity if they are not available

--- src/features/pointVisualization/components/methodPicker.tsx
+++ src/features/pointVisualization/components/methodPicker.tsx
@@ -3,13 +3,21 @@ import { useTranslation } from "react-i18next";
 import { useHistory, useLocation } from "react-router-dom";
 
 import { useAppDispatch } from "app/redux-store-interactions";
+import { Tooltip } from "components";
+import { useExplorerGlobals } from "contexts/explorerGlobals";
 import { renderActions } from "features/render";
 
 export function MethodPicker() {
+    const {
+        state: { view },
+    } = useExplorerGlobals(true);
     const { t } = useTranslation();
     const location = useLocation();
     const history = useHistory();
     const dispatch = useAppDispatch();
+
+    const hasClassification = view.renderState.scene?.config.hasPointClassification ?? false;
+    const hasIntensity = view.renderState.scene?.config.hasPointIntensity ?? false;
 
     return (
         <Tabs
@@ -19,9 +27,17 @@ export function MethodPicker() {
                 dispatch(renderActions.setPoints({ defaultPointVisualization: { kind: path.slice(1) } }));
             }}
         >
-            <Tab value="/classification" label={t("classification")} />
+            <Tooltip title={hasClassification ? "" : t("projectDoesNotHaveClassificationData")}>
+                <div>
+                    <Tab value="/classification" label={t("classification")} disabled={!hasClassification} />
+                </div>
+            </Tooltip>
             <Tab value="/elevation" label={t("elevation")} />
-            <Tab value="/intensity" label={t("intensity")} />
+            <Tooltip title={hasClassification ? "" : t("projectDoesNotHaveIntensityData")}>
+                <div>
+                    <Tab value="/intensity" label={t("intensity")} disabled={!hasIntensity} />
+                </div>
+            </Tooltip>
             <Tab value="/color" label={t("rgb")} />
         </Tabs>
     );
