78384b37d4c16e66a53b99f08f13a34551400053
nd-novorender <nd@novorender.com>
2024-10-15 14:33:57 +0200

    Show simple gizmo when hovering with outline laser

--- src/features/render/svgUtils.ts
+++ src/features/render/svgUtils.ts
@@ -1,5 +1,5 @@
 import { View } from "@novorender/api";
-import { quat, vec3 } from "gl-matrix";
+import { quat, vec2, vec3 } from "gl-matrix";
 
 import { isRealVec } from "utils/misc";
 
@@ -37,7 +37,7 @@ export function moveSvgCursor({
           }
         | undefined;
     color: string;
-    overrideKind: "edge" | "corner" | "surface" | "cross" | undefined;
+    overrideKind: "edge" | "corner" | "surface" | "cross" | "gizmo" | undefined;
 }) {
     if (!svg || !view) {
         return;
@@ -54,6 +54,38 @@ export function moveSvgCursor({
         g.innerHTML = `<path d="M-10,0L10,0M0,-10L0,10" stroke-width="2" stroke-linecap="round" stroke="${
             color || "red"
         }"/>`;
+    } else if (overrideKind === "gizmo" && pickResult) {
+        const pos = pickResult.position;
+        const z0 = pos;
+        const z1 = vec3.add(vec3.create(), pos, pickResult.normal);
+        const xDir = vec3.cross(vec3.create(), pickResult.normal, vec3.fromValues(0, 0, 1));
+        vec3.normalize(xDir, xDir);
+        const x0 = vec3.add(vec3.create(), pos, xDir);
+        const x1 = vec3.scaleAndAdd(vec3.create(), pos, xDir, -1);
+        const yDir = vec3.cross(vec3.create(), pickResult.normal, xDir);
+        vec3.normalize(yDir, yDir);
+        const y0 = vec3.add(vec3.create(), pos, yDir);
+        const y1 = vec3.scaleAndAdd(vec3.create(), pos, yDir, -1);
+        const [sx0, sx1, sy0, sy1, sz0, sz1] = view.convert.worldSpaceToScreenSpace([x0, x1, y0, y1, z0, z1]);
+
+        if (!sz0) {
+            return;
+        }
+
+        const drawPath = (p0: vec2 | undefined, p1: vec2 | undefined, color: string) => {
+            if (!p0 || !p1) {
+                return "";
+            }
+            p0 = vec2.sub(vec2.create(), p0, sz0);
+            p1 = vec2.sub(vec2.create(), p1, sz0);
+            return `<path d="M${p0[0]} ${p0[1]}L${p1[0]} ${p1[1]}" stroke-width="2" stroke-linecap="round" stroke="${color}" />`;
+        };
+
+        g.innerHTML = [
+            drawPath(sx0, sx1, "#8BC34A"), // green
+            drawPath(sy0, sy1, "#03A9F4"), // blue
+            drawPath(sz0, sz1, "#F44336"), // red
+        ].join("");
     } else {
         const kind = (overrideKind ? overrideKind : pickResult?.sampleType) ?? "corner";
 
@@ -79,7 +111,7 @@ export function moveSvgCursor({
                 g.innerHTML = `<circle r="20" fill="rgba(255,255,255,0.25)" ${style}/><line x2="${(
                     normal[0] * 20
                 ).toFixed(1)}" y2="${(normal[1] * -20).toFixed(
-                    1
+                    1,
                 )}" stroke=${color} stroke-width="2" stroke-linecap="round" />`;
             } else {
                 g.innerHTML = `<circle r="20" fill="rgba(255,255,255,0.25)" /><circle r="2" fill=${color} />`;
