360029432fef5616d67536334f64efedd76b31a7
nd-novorender <nd@novorender.com>
2024-10-18 12:42:24 +0200

    Update shareLink

--- src/features/shareLink/useShareLink.ts
+++ src/features/shareLink/useShareLink.ts
@@ -1,3 +1,4 @@
+import { ExplorerBookmarkState } from "@novorender/data-js-api";
 import { useCallback } from "react";
 
 import { useSaveBookmarksMutation } from "apis/dataV2/dataV2Api";
@@ -9,45 +10,48 @@ export function useShareLink() {
     const [saveBookmarks] = useSaveBookmarksMutation();
     const sceneId = useSceneId();
 
-    return useCallback(async () => {
-        const id = window.crypto.randomUUID();
-        const bm = createBookmark();
+    return useCallback(
+        async (explorerStateOverwrite: Partial<ExplorerBookmarkState> = { forms: undefined }) => {
+            const id = window.crypto.randomUUID();
+            const bm = createBookmark(undefined, explorerStateOverwrite);
 
-        const blob = new Blob([`${window.location.origin}${window.location.pathname}?bookmarkId=${id}`], {
-            type: "text/plain",
-        });
-        let saved = false;
+            const blob = new Blob([`${window.location.origin}${window.location.pathname}?bookmarkId=${id}`], {
+                type: "text/plain",
+            });
+            let saved = false;
 
-        try {
-            // Safari treats user activation differently:
-            // https://bugs.webkit.org/show_bug.cgi?id=222262.
-            await navigator.clipboard.write([
-                new ClipboardItem({
-                    "text/plain": (async () => {
-                        await saveBookmarks({
-                            projectId: sceneId,
-                            bookmarks: [{ ...bm, id, name: id }],
-                            group: id,
-                        }).unwrap();
+            try {
+                // Safari treats user activation differently:
+                // https://bugs.webkit.org/show_bug.cgi?id=222262.
+                await navigator.clipboard.write([
+                    new ClipboardItem({
+                        "text/plain": (async () => {
+                            await saveBookmarks({
+                                projectId: sceneId,
+                                bookmarks: [{ ...bm, id, name: id }],
+                                group: id,
+                            }).unwrap();
 
-                        saved = true;
-                        return blob;
-                    })(),
-                }),
-            ]);
-        } catch (e) {
-            if (!saved) {
-                console.warn(e);
-                return;
+                            saved = true;
+                            return blob;
+                        })(),
+                    }),
+                ]);
+            } catch (e) {
+                if (!saved) {
+                    console.warn(e);
+                    return;
+                }
+
+                navigator.clipboard.write([
+                    new ClipboardItem({
+                        [blob.type]: blob,
+                    }),
+                ]);
             }
 
-            navigator.clipboard.write([
-                new ClipboardItem({
-                    [blob.type]: blob,
-                }),
-            ]);
-        }
-
-        return saved;
-    }, [createBookmark, saveBookmarks, sceneId]);
+            return saved;
+        },
+        [createBookmark, saveBookmarks, sceneId],
+    );
 }
